96639f809a0854d51ce222b5ea77fa44cf229547
Andrey <panchenko.a91@gmail.com>
2018-10-23 00:06:42 +0300

    #3

--- src/app/js/common.js
+++
@@ -1,7 +0,0 @@
-(function () {
-    $('.hamburger').on('click', function () {
-        $('.hamburger').toggleClass('is-active');
-        $('.mobile-mnu').toggleClass('active');
-        $('.logo').toggleClass('hide');
-    })
-})();
