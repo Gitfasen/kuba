f09635657184eed4e45f246be669dfb7b73ed363
Andrey <panchenko.a91@gmail.com>
2018-10-23 23:37:47 +0300

    #4

--- app/js/common.js
+++ app/js/common.js
@@ -3,7 +3,10 @@
         $('.hamburger').toggleClass('is-active');
         $('.mobile-mnu').toggleClass('active');
         $('.logo').toggleClass('hide');
-    })
+        $('.video-bg_layer').toggleClass('hide');
+        $('.home-layer-info').toggleClass('hide');
+    });
+
     $('.header-video').vide({
         mp4: 'video/1.mp4',
     });
