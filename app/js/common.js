30a2309c984b754621a10659064d4272aebbcc77
Andrey <panchenko.a91@gmail.com>
2018-10-25 00:57:40 +0300

    #5

--- app/js/common.js
+++ app/js/common.js
@@ -10,4 +10,25 @@
     $('.header-video').vide({
         mp4: 'video/1.mp4',
     });
+
+    // Video Popup
+    $(".js-modal-video").modalVideo({
+        channel:'youtube',
+        autoplay: 1
+    });
+
+    if ($('.content').is('.scroll')) {
+        $('body').css('overflow', 'scroll');
+    }
+
+    $.jInvertScroll(['.scroll'], {
+        width: 'auto',	// Page width (auto or int value)
+        height: 3000,	// Page height (the shorter, the faster the scroll)
+        onScroll: function(percent) {
+            // Callback function that will be called each time the user
+            // scrolls up or down, useful for animating other parts
+            // on the page depending on how far the user has scrolled down
+            // values go from 0.0 to 1.0 (with 4 decimals precision)
+        }
+    });
 })();
